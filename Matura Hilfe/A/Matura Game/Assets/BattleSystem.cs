6fee1f1d98ec6f54aaaa798ad07cbbb3d6c7ec74
DaveGameDevelopment <skillbasedstudios@gmail.com>
2022-09-26 20:00:47 +0200

    Animation AttackButtons

--- Matura Hilfe/A/Matura Game/Assets/BattleSystem.cs
+++ Matura Hilfe/A/Matura Game/Assets/BattleSystem.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-public enum BattleState {start, Playerturn, Enemyturn, won, lost}
+using DG.Tweening; // <-- damit du DoTween benutzen kannst
 
 public class BattleSystem : MonoBehaviour
 {
@@ -12,10 +11,12 @@ public class BattleSystem : MonoBehaviour
     public Transform playerSpawn;
     public Transform enemySpawn;
     public Transform infoSpawn;
+     
+    private Unit PlayerUnit;
+    private Unit EnemyUnit;
 
-    Unit PlayerUnit;
-    Unit EnemyUnit;
-    
+    public enum BattleState { start, Playerturn, Enemyturn, won, lost }
+
     public BattleState state;
     public CharHUD playerHUD;
     public CharHUD enemyHUD;
@@ -23,11 +24,34 @@ public class BattleSystem : MonoBehaviour
     public Text StateText;
     public Text Information;
 
+    [Header("ByDave")]
+    public Transform container_attackButtons;
+    public float attackButtonAnimSpeed;
+    public float attackButtonYHideRange;
+    private Vector3 originalAttackButtonsPos;
+
     void Start()
     {  
         state = BattleState.start;
+
+        attackButtonsShown = true;
+        // MoveAttackButtons(false);
+
         SetupBattle();
         StartCoroutine(SetupBattle());
+    }
+
+    private string lastInformation;
+    private void Update()
+    {
+        // bischen weird das so zu coden, aber egal
+        /// sobald sich der Informations text zu "" AttackButtons angezeigt
+        
+        // text hat sich geändert
+        if(lastInformation != Information.text)
+            MoveAttackButtons(Information.text == "");
+
+        lastInformation = Information.text;
     }
 
     IEnumerator SetupBattle()
@@ -57,7 +81,7 @@ public class BattleSystem : MonoBehaviour
 
     IEnumerator PlayerAttack()
     {
-        bool isDead=EnemyUnit.TakeDamage(PlayerUnit.damage);
+        bool isDead = EnemyUnit.TakeDamage(PlayerUnit.damage);
         enemyHUD.SetHP(EnemyUnit.currentHP);
         
         Information.text="You have done "+ PlayerUnit.damage +" Damage!";
@@ -75,7 +99,6 @@ public class BattleSystem : MonoBehaviour
             Information.text="";
             StartCoroutine(Enemyturn());
         }
-
     }
 
     IEnumerator Enemyturn()
@@ -120,4 +143,27 @@ public class BattleSystem : MonoBehaviour
         
         StartCoroutine(PlayerAttack());
     }
+
+    #region AttackButton Animation
+
+    private bool attackButtonsShown;
+    public void MoveAttackButtons(bool show)
+    {
+        // wenn sie schon angezeigt sind und man sie anzeigen will -> funktion stoppen (return)
+        if (attackButtonsShown == show) return;
+
+        Vector3 direction = show ? Vector3.up : Vector3.down;
+        Vector3 adjustedPos = container_attackButtons.position + direction * attackButtonYHideRange;
+
+        //container_attackButtons.position = adjustedPos;
+
+        attackButtonsShown = show;
+
+        print(show);
+        container_attackButtons.DOKill();
+        container_attackButtons.DOMove(adjustedPos, attackButtonAnimSpeed);
+        //online = show;
+    }
+
+    #endregion
 }
